ac768a15091af78523a7609fe7c9aea6e444c7a9
Felix Enriquez <efelixagustin@gmail.com>
2017-12-21 16:29:09 -0300

    add browser support

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const abis = require('./contracts')
-const Web3 = require('web3');
+const Web3 = require('web3')
 
 const getInstance = provider => {
-    const web3 = new Web3(Web3.givenProvider || provider);
+  const web3 = new Web3(Web3.givenProvider || provider)
 
-    const res = {
-        proceeds: new web3.eth.Contract(abis.Proceeds),
-        mtntoken: new web3.eth.Contract(abis.MTNToken),
-        auctions: new web3.eth.Contract(abis.Auctions),
-        smartToken: new web3.eth.Contract(abis.SmartToken),
-        autonomousConverter: new web3.eth.Contract(abis.AutonomousConverter)
-    }
+  const res = {
+    proceeds: new web3.eth.Contract(abis.Proceeds),
+    mtntoken: new web3.eth.Contract(abis.MTNToken),
+    auctions: new web3.eth.Contract(abis.Auctions),
+    smartToken: new web3.eth.Contract(abis.SmartToken),
+    autonomousConverter: new web3.eth.Contract(abis.AutonomousConverter)
+  }
 
-    for (let key in res) {
-        res[key].setProvider(provider)
-    }
+  for (let key in res) {
+    res[key].setProvider(provider)
+  }
 
-    return Object.assign({}, res, {
-        web3
-    })
+  return Object.assign({}, res, {
+    web3
+  })
 }
 
 module.exports = {
-    getInstance
+  getInstance
 }
