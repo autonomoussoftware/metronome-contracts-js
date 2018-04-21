38a8ca2ad58de6759e9bd025e465a12cdc6a8a53
Ignacio  Anaya <ignacio.anaya89@gmail.com>
2018-04-21 17:05:32 -0300

    Update contracts abi's

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const abis = require('./contracts')
+const metTokenAbi = require('./contracts/METToken')
+const auctionsAbi = require('./contracts/Auctions')
+const autonomousConverterAbi = require('./contracts/AutonomousConverter')
 
 class Metronome {
   constructor (web3) {
     const res = {
-      mtntoken: new web3.eth.Contract(abis.MTNToken),
-      auctions: new web3.eth.Contract(abis.Auctions),
-      autonomousConverter: new web3.eth.Contract(abis.AutonomousConverter)
+      mtntoken: new web3.eth.Contract(metTokenAbi),
+      auctions: new web3.eth.Contract(auctionsAbi),
+      autonomousConverter: new web3.eth.Contract(autonomousConverterAbi)
     }
 
     return res
