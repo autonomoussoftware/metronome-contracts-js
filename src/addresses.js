d182e97e4869f3ca2c591a29ce851ca26627baf1
Gabriel Montes <gabriel@bloq.com>
2019-01-08 11:50:50 -0300

    Document contracts commit hash

--- src/addresses.js
+++ src/addresses.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// autonomoussoftware/metronome#35a6cd3
 const mainnet = {
+  // autonomoussoftware/metronome#35a6cd3
   auctions: '0x9d9BcDd249E439AAaB545F59a33812E39A8e3072',
   autonomousConverter: '0x686e5ac50D9236A9b7406791256e47feDDB26AbA',
   metToken: '0xa3d58c4E56fedCae3a7c43A725aeE9A71F0ece4e',
@@ -9,11 +9,12 @@ const mainnet = {
   validator: '0xa5474b3ee0f86f75528b525c37440d61665f93c4'
 }
 
-// autonomoussoftware/metronome#80364c4
 const ropsten = {
+  // autonomoussoftware/metronome#80364c4
   auctions: '0x767182C6ef62398993099e5542Ab43c2456A8abA',
   autonomousConverter: '0x638E84db864AA345266e1AEE13873b860aFe82e7',
   metToken: '0xF3e9a687Fdf24112745D4d7dEe150BA87A07ecc3',
+  // metronome#81d451a
   tokenPorter: '0x86a6e9cbf616f157e33f4b95fa686eb3e0d80b50',
   validator: '0x73924d3c6dbabf47fffa2d9f9303dc2049aec44a'
 }
