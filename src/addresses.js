c19610a610cc913e1348d2bb0d0845ad4c78e366
Gabriel Montes <gabriel@bloq.com>
2019-01-10 15:37:00 -0300

    Add Morden contracts

--- src/addresses.js
+++ src/addresses.js
@@ -14,17 +14,29 @@ const ropsten = {
   auctions: '0x767182C6ef62398993099e5542Ab43c2456A8abA',
   autonomousConverter: '0x638E84db864AA345266e1AEE13873b860aFe82e7',
   metToken: '0xF3e9a687Fdf24112745D4d7dEe150BA87A07ecc3',
-  // metronome#81d451a
+  // internal/metronome#81d451a
   tokenPorter: '0x86a6e9cbf616f157e33f4b95fa686eb3e0d80b50',
   validator: '0x73924d3c6dbabf47fffa2d9f9303dc2049aec44a'
 }
 
+const morden = {
+  // internal/metronome#bee672b (TBC)
+  auctions: '0xb7f717948ce7b91e234de93f6edaba8d314b8685',
+  autonomousConverter: '0x60942c9d3775c4d71ac595f92fab1e0cd1182020',
+  metToken: '0x9b9fb9bbcf19b046d02cd6b49207ad2466ec7a6d',
+  tokenPorter: '0x75f8c2e7cee21c2de8d5105ed68ece297425c1d3',
+  validator: '0x874167a53c019279cbec02fa23cde867db2ae32a'
+}
+
 const addresses = {
   1: mainnet,
+  2: morden,
   3: ropsten,
+  62: morden,
   main: mainnet,
   mainnet,
-  ropsten
+  ropsten,
+  morden
 }
 
 module.exports = addresses
