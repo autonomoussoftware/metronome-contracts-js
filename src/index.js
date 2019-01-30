0aeb7724e525ff535deb5e18fb15f7e4b302bcfd
Gabriel Montes <gabriel@bloq.com>
2019-01-30 12:29:56 -0300

    Update lint rules, tests and doc

--- src/index.js
+++ src/index.js
@@ -5,32 +5,53 @@ const mapValues = require('../lib/map-values')
 const aliases = require('./aliases.json')
 const contractsDefinition = require('./contracts.json')
 
-const allContracts = mapValues(aliases, alias =>
-  mapValues(
-    contractsDefinition[alias].contracts,
-    ({ address, birthblock, version }, name) => ({
-      abi: require(`./abis/${version}/${name}.json`),
-      address,
-      birthblock: birthblock || contractsDefinition[alias].birthblock
-    })
+// Create an object containing all chains, its contracts and static data.
+const allContracts = mapValues(aliases, function (alias) {
+  const definitions = contractsDefinition[alias]
+  return mapValues(
+    definitions.contracts,
+    ({ address, birthblock, version }, name) =>
+      ({
+        abi: require(`./abis/${version || definitions.version}/${name}.json`),
+        address,
+        birthblock: birthblock || definitions.birthblock
+      })
   )
-)
+})
 
+/**
+ * Create a contract using either web3@0.2x or web3@1.0.0
+ *
+ * @param {Web3} web3 The Web3 instance to instantiate the contracts.
+ * @param {Array} abi The contract's ABI.
+ * @param {string} address The contract's address.
+ * @returns {*} The contract instance.
+ */
 const createContract = (web3, abi, address) =>
   typeof web3.eth.Contract === 'function'
     ? new web3.eth.Contract(abi, address)
     : web3.eth.contract(abi).at(address)
 
+/** Class representing a contracts set. */
 class MetronomeContracts {
-  constructor (web3, _chain = 'mainnet') {
+  /**
+   * Create a Metronome contracts set.
+   *
+   * The contract set contains instances of all the contracts at the proper
+   * addresses depending on the target chain.
+   *
+   * @param {Web3} web3 The Web3 instance to instantiate the contracts.
+   * @param {string} [chain='mainnet'] The target chain name or ID.
+   */
+  constructor (web3, chain = 'mainnet') {
     if (!web3 || !web3.eth) {
-      throw new Error('Invalid web3 instance or not supplied')
+      throw new Error('Invalid web3 provided')
     }
 
-    const contracts = allContracts[aliases[_chain]]
+    const contracts = allContracts[aliases[chain]]
 
     if (!contracts) {
-      throw new Error('Unknown "chain" parameter')
+      throw new Error('Unknown chain')
     }
 
     Object.assign(this, mapValues(contracts, ({ abi, address }) =>
@@ -39,6 +60,7 @@ class MetronomeContracts {
   }
 }
 
+// Add contract definitions to the MetronomeContracts class for convenience
 Object.assign(MetronomeContracts, allContracts)
 
 module.exports = MetronomeContracts
---
+++ test/index.test.js
@@ -0,0 +1,92 @@
+'use strict'
+
+const Web3 = require('web3')
+const web3 = new Web3()
+
+const MetronomeContracts = require('../src')
+
+const ROPSTEN = 'ropsten'
+
+const contracts = {
+  mainnet: {
+    Auctions: {
+      address: '0x9d9BcDd249E439AAaB545F59a33812E39A8e3072',
+      birthblock: 5659894
+    },
+    AutonomousConverter: {
+      address: '0x686e5ac50D9236A9b7406791256e47feDDB26AbA'
+    },
+    METToken: {
+      address: '0xa3d58c4E56fedCae3a7c43A725aeE9A71F0ece4e'
+    }
+  },
+  ropsten: {
+    Auctions: {
+      address: '0x767182C6ef62398993099e5542Ab43c2456A8abA',
+      birthblock: 3399250
+    },
+    AutonomousConverter: {
+      address: '0x638E84db864AA345266e1AEE13873b860aFe82e7'
+    },
+    METToken: {
+      address: '0xF3e9a687Fdf24112745D4d7dEe150BA87A07ecc3'
+    }
+  }
+}
+
+test('initializes contracts with default chain', function () {
+  const metronome = new MetronomeContracts(web3)
+
+  expect(metronome.METToken)
+    .toBeDefined()
+  expect(metronome.Auctions)
+    .toBeDefined()
+  expect(metronome.AutonomousConverter)
+    .toBeDefined()
+})
+
+test('initializes contracts for specifi', function () {
+  const metronome = new MetronomeContracts(web3, ROPSTEN)
+
+  expect(metronome.METToken)
+    .toBeDefined()
+  expect(metronome.Auctions)
+    .toBeDefined()
+  expect(metronome.AutonomousConverter)
+    .toBeDefined()
+})
+
+test('initializes contracts and get the addresses on Ropsten', function () {
+  const metronome = new MetronomeContracts(web3, ROPSTEN)
+
+  expect(metronome.METToken.options.address.toLowerCase())
+    .toBe(contracts[ROPSTEN].METToken.address.toLowerCase())
+  expect(metronome.Auctions.options.address.toLowerCase())
+    .toBe(contracts[ROPSTEN].Auctions.address.toLowerCase())
+  expect(metronome.AutonomousConverter.options.address.toLowerCase())
+    .toBe(contracts[ROPSTEN].AutonomousConverter.address.toLowerCase())
+})
+
+test('initializes contracts and check some methods', function () {
+  const metronome = new MetronomeContracts(web3, ROPSTEN)
+
+  expect(typeof metronome.METToken.methods.transfer)
+    .toBe('function')
+  expect(typeof metronome.Auctions.methods.heartbeat)
+    .toBe('function')
+  expect(typeof metronome.AutonomousConverter.methods.convertEthToMet)
+    .toBe('function')
+})
+
+test('initializes contracts with invalid chain and throw error', function () {
+  try {
+    new MetronomeContracts(web3, 'fakenet') // eslint-disable-line no-new
+  } catch (err) {
+    expect(err.message)
+      .toBe('Unknown chain')
+  }
+})
+
+test('check static properties as addresses and birthblocks', function () {
+  expect(Object.assign({}, MetronomeContracts)).toMatchObject(contracts)
+})
